2cffd092145bb0ab3a8590436be55b75aa9e4c7d
Nomaan Shaikh <nomaan.shaikh10@gmail.com>
2020-02-02 10:17:07 -0800

    new file

--- test.cpp
+++ test.cpp
@@ -1,39 +1,3 @@
 #include<iostream>
 
-using namespace std;
-
-int sub(int x, int y)
-{
-    return x-y;
-}
-int add(int a, int b)
-{
-    return a+b;
-}
-int div(int a, int b)
-{
-    return a/b;
-}
-int multiply(int a,int b)
-{
-    return a*b;
-}
-int remainder(int a,int b)
-{
-    return a%b;
-}
-int main() 
-{
-    cout<<sub(10,2);
-    int a=5,b=5;
-    cout<<add(a,b)<<endl;
-    //Add multiply - Nomaan
-    cout<<div(55,5);
-    //Nomaan - BRANCH123
-    cout<<multiply(a,b);
-    //Add divie - Devam
-
-    //Added Nomaan
-    cout<<remainder(11,5);
-}
 
