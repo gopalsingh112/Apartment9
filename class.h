368e36e4e78539a8b92ebfdb8a3d5174a90b02c3
Nomaan Shaikh <nomaan.shaikh10@gmail.com>
2020-02-02 16:50:27 -0800

    Add function

---
+++ class.h
@@ -0,0 +1,10 @@
+class calc{
+    public:
+        int add(int a, int b)
+        {
+            return a+b;
+        }
+    private:
+        int val1;
+        int val2;
+};
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "class.h"
+
+using namespace std;
+
+int main()
+{
+    calc obj;
+    cout<<obj.add(5,6)<<endl;
+}
