5e64cbe33578736b6e02033ef0cacb6c2d0ef9a0
Nomaan Shaikh <nomaan.shaikh10@gmail.com>
2020-02-03 09:55:06 -0800

    Subtract

--- class.h
+++ class.h
@@ -4,6 +4,10 @@ class calc{
         {
             return a+b;
         }
+        int sub(int a,int b)
+        {
+            return a-b;
+        }
     private:
         int val1;
         int val2;
--- main.cpp
+++ main.cpp
@@ -7,4 +7,5 @@ int main()
 {
     calc obj;
     cout<<obj.add(5,6)<<endl;
+    cout<<obj.sub(10,2)<<endl;
 }
